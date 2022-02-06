8d97ebfd42c7821f5c4176937c8ecce69de650d4
AtomHeartFather <quito@mail.ru>
2022-02-07 02:09:36 +0300

    API инкрементирования лайков

--- app/Http/Controllers/myAPI.php
+++ app/Http/Controllers/myAPI.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class myAPI extends Controller
 {
-    function getData() {
+    function putLikes(Request $request) {
 
-        return ['name' => 'egor'];
+        $article = Article::firstwhere('id', $request->article_id);
+        $article->likes += 1;
+        $article->save();
+        return ['likes' => $article->likes ];
 
     }
 }
